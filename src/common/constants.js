629640d5a6aaf17c972e7c666ab7cfb30ba973f2
JeremyLoh <jeremyloh03@gmail.com>
2021-05-10 22:00:21 +0800

    Add attribution and map tile url constants

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,8 +2,12 @@ const API_URL = "https://api.wheretheiss.at/v1/";
 const SATELLITES_ENDPOINT = "https://api.wheretheiss.at/v1/satellites/";
 const COORDINATES_ENDPOINT = "https://api.wheretheiss.at/v1/coordinates/";
 const ISS_NORAD_CATALOG_ID = "25544";
+const OPENSTREETMAP_ATTRIBUTION = "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors";
+const TILE_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
 module.exports.API_URL = API_URL;
 module.exports.SATELLITES_ENDPOINT = SATELLITES_ENDPOINT;
 module.exports.COORDINATES_ENDPOINT = COORDINATES_ENDPOINT
 module.exports.ISS_NORAD_CATALOG_ID = ISS_NORAD_CATALOG_ID;
+module.exports.OPENSTREETMAP_ATTRIBUTION = OPENSTREETMAP_ATTRIBUTION;
+module.exports.TILE_URL = TILE_URL;
